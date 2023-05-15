b6f7546c474a6b07eba0d05bf1eb6b42096dfaa6
maria-molinal <123768478+maria-molinal@users.noreply.github.com>
2023-05-15 12:44:36 -0500

    mk,´p

---
+++ app.js
@@ -0,0 +1,17 @@
+const cuadrado = document.querySelectorAll(".cuadrado");
+const tiempofaltante = document.querySelector("#tiempo");
+let puntaje = document.getElementById("puntaje");
+
+let resultado = 0;
+let tiempoactual = tiempofaltante.textContent; 
+ 
+function cuadradoazar {
+  function cuadradoazar {
+    cuadrado.forEach(nombredeclase => {
+    nombredeclase.classList.remove("topo");
+  })
+  let posicionalazar = cuadrado  [Math.floor (Math.randon() * 9)]
+  posicionalazar.classList.add("topo");
+  posiciontopo.posicionalazar.Id
+  }
+}
